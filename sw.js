70825ee0dcde0b417f946df1b4760500333d125c
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-07 23:44:18 +0300

    Refresh Nova tab assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nova-main-v8';
+const CACHE='nova-main-v9';
 const FILES=['./','./index.html','./app.css','./app.js','./theme.js','./balance.css','./balance.js','./sections.css','./sections.js','./reference.css','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

39fbeade5252ae3d927eb52d075e4daf490db800
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-07 22:17:41 +0300

    Cache theme switch with new main screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='health-main-v2';
-const FILES=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
+const CACHE='health-main-v3';
+const FILES=['./','./index.html','./app.css','./app.js','./theme.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

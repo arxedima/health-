7a7f3425e7335f561df96e7b2a7e34530aa24ddb
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-07 22:16:43 +0300

    Refresh cache for new Health+ main screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nova-exact-v1';
+const CACHE='health-main-v2';
 const FILES=['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

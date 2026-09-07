d06403f9a53309f3fe6492a6abb806511feabf2a
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-07 22:24:42 +0300

    Refresh Nova+ cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='health-main-v3';
+const CACHE='nova-main-v4';
 const FILES=['./','./index.html','./app.css','./app.js','./theme.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));

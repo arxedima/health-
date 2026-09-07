caf7917684f318a266d9b467b3da9ad71c524ef0
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-07 23:24:55 +0300

    Cache Nova health section screens

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='nova-main-v6';
-const FILES=['./','./index.html','./app.css','./app.js','./theme.js','./balance.css','./balance.js','./manifest.webmanifest'];
+const CACHE='nova-main-v7';
+const FILES=['./','./index.html','./app.css','./app.js','./theme.js','./balance.css','./balance.js','./sections.css','./sections.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

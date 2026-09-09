bd0896ff7ef46c1455778e9e49d73213367c9552
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-09 18:05:36 +0300

    IRIS v0.9 cache update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='iris-v0.8.0';
-const CORE=['./','./index.html','./styles.css?v=iris08','./iris-engine.js?v=iris08','./manifest.webmanifest?v=iris08','./iris-icon.svg?v=iris08'];
+const CACHE='iris-v0.9.0';
+const CORE=['./','./index.html','./styles.css?v=iris09','./iris-engine.js?v=iris09','./manifest.webmanifest?v=iris09','./iris-icon.svg?v=iris09'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

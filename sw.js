3d39b415f2f4b46058dc977b041917ad6548bef9
arxedima <152386712+arxedima@users.noreply.github.com>
2026-09-09 12:19:18 +0300

    IRIS v0.4: bump PWA cache

--- sw.js
+++ sw.js
@@ -1,8 +1,32 @@
-const CACHE='iris-v0.3.0';
-const CORE=['./','./index.html','./styles.css?v=iris03','./iris-engine.js?v=iris03','./manifest.webmanifest?v=iris03','./iris-icon.svg?v=iris03'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+const CACHE = 'iris-v0.4.0';
+const CORE = [
+  './',
+  './index.html',
+  './styles.css?v=iris04',
+  './iris-engine.js?v=iris04',
+  './manifest.webmanifest?v=iris04',
+  './iris-icon.svg?v=iris04'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((res) => res || caches.match('./index.html')))
+  );
 });
